2c448dab04084ebe5ac6396bff1d88e80ca94795
Matthew Grasmick <matt.grasmick@gmail.com>
2020-01-22 10:17:16 -0500

    Use MessengerTrait instead.

--- src/Form/StripePlanEntityForm.php
+++ src/Form/StripePlanEntityForm.php
@@ -2,13 +2,9 @@
 
 namespace Drupal\stripe_registration\Form;
 
-use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\ContentEntityForm;
-use Drupal\Core\Entity\EntityRepositoryInterface;
-use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Messenger\MessengerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Messenger\MessengerTrait;
 
 /**
  * Form controller for Stripe plan edit forms.
@@ -16,41 +12,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @ingroup stripe_registration
  */
 class StripePlanEntityForm extends ContentEntityForm {
-  /**
-   * The messenger service.
-   *
-   * @var \Drupal\Core\Messenger\MessengerInterface
-   */
-  protected $messenger;
 
-  /**
-   * Constructor for StripePlanEntityForm.
-   *
-   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   *   The messenger service.
-   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
-   *   The entity repository service.
-   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
-   *   The entity type bundle service.
-   * @param \Drupal\Component\Datetime\TimeInterface $time
-   *   The time service.
-   */
-  public function __construct(MessengerInterface $messenger, EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL) {
-    parent::__construct($entity_repository, $entity_type_bundle_info, $time);
-    $this->messenger = $messenger;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('messenger'),
-      $container->get('entity.repository'),
-      $container->get('entity_type.bundle.info'),
-      $container->get('datetime.time')
-    );
-  }
+  use MessengerTrait;
 
   /**
    * {@inheritdoc}
